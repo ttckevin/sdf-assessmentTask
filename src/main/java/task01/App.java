2ec97bf7e6d1d606b32d535a37933ca422936a9d
ttckevin <sl33pydr4g0n@gmail.com>
2022-07-01 15:06:06 +0800

    adding gitIgnore

--- src/main/java/task01/App.java
+++ src/main/java/task01/App.java
@@ -1,13 +1,48 @@
 package task01;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
+public class App {
+    public static void main( String[] args ) throws IOException {
+
+        String path = "C:/Users/T1me/Desktop/codingRevision/assessmentTask1/tour_packages.csv";
+        String line = " ";
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(path));
+
+            while ((line = br.readLine()) != null ){
+                // System.out.println(line);
+                String [] column = line.split(",");
+                List<String> mylist = Arrays.asList(column);
+                //print the list
+                System.out.println("");
+                for(String a : mylist){
+                   System.out.print(a + " ");
+                //This should solve the read file issue
+                }
+            for(int i=0; i<=column.length;i++){
+                if(i<)
+            }
+            //Store the words under the specific headers 
+            //Replace the column with row
+            //Read Files
+            //Everytime the program reads a word with the specific header it can replace it with a word that is available
+
+                }
+
+
+            }catch (FileNotFoundException e){
+            e.printStackTrace();
+        }
     }
 }
+
